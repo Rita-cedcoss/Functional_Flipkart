738f2d87eec31d55a81663fe783d2c7de2894a0d
Rita-cedcoss <ritapal@cedcoss.com>
2022-08-31 18:04:39 +0530

    functional Flipkart task completed

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import './Functional_Flipkart/Component.css'
+import Component1 from './Functional_Flipkart/Component1';
+import Component2 from './Functional_Flipkart/Component2';
+import Component3 from './Functional_Flipkart/Component3';
+import Component4 from './Functional_Flipkart/Component4';
+
+function App() {
+  return (
+   <>
+     <Component1></Component1>
+     <Component2></Component2>
+     <Component3></Component3>
+     <Component4></Component4>
+   </>
+  );
+}
+export default App;
